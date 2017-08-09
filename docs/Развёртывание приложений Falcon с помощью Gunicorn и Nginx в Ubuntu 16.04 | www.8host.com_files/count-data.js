f1e758653bcbca2075b13bd6fcc0118c110d5e77
Osin <osinpaul@gmail.com>
2017-08-09 15:01:41 +0300

    2. Installed venv inside the project folder

---
+++ docs/Развёртывание приложений Falcon с помощью Gunicorn и Nginx в Ubuntu 16.04 | www.8host.com_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"\u0438","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"2979 https:\/\/www.8host.com\/blog\/?p=2979","comments":0}]});
+}
